5c2fb629fd884c50025bf0b71e6726629c471b47
caminar123 <beymarpierrechoquecuba@gmail.com>
2025-10-07 21:29:44 -0400

    Add files via upload

---
+++ JAVA_SCRIPT/java_scr.js
@@ -0,0 +1,37 @@
+
+
+ let numerosecreto=6;
+ let numeros=prompt("indica el numero");
+ if (numerosecreto==numeros){
+    alert("Acertaste el numero secreto");
+ } else{
+    alert("Fallaste, el numero secreto es ");
+ }
+
+//3
+let i2= prompt("indica un numero");
+let doble=i2*2;
+alert("El doble de "+i2+" es "+doble);
+
+
+
+//4
+let num1=prompt("indica el primer numero");
+let num2=prompt("indica el segundo numero");
+let num3=prompt("indica el tercer numero"); 
+
+let resultado=(parseInt(num1)+parseInt(num2)+parseInt(num3))/3;
+alert("El promedio de "+num1+", "+num2+" y "+num3+" es "+resultado);
+
+
+//5
+let a1=prompt("indica el primer numero");
+let a2=prompt("indica el segundo numero");
+let mayorresultado=(parseInt(a1)>parseInt(a2))?a1:a2;
+alert("El mayor entre "+a1+" y "+a2+" es "+mayorresultado);
+
+//6
+let i1=prompt("indica el numero");
+let cuadradoresultado=parseInt(i1)*parseInt(i1);
+alert("El cuadrado de "+i1+" es "+cuadradoresultado);   
+
